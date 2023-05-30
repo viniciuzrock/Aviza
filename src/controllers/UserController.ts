d9103d61c56060a2d9f4f03d9b04f8de941d3da9
viniciuzrock <vinir.santoss@gmail.com>
2023-05-30 16:02:08 -0300

    WIP - criando usuario com firebase

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,31 +1,52 @@
 import { Router, Request, Response } from "express"
-import firebaseAuth, { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
 import firebaseApp from "../../config/firebase";
 
 class UserController {
 
     static async register(req: Request, res: Response) {
-        res.status(200).send({
-            message: "User registered"
-        })
+        try {
+            const { email, password } = req.body;
+            // const auth = firebaseAuth.getAuth(firebaseApp)
+            const auth = getAuth()
+            await createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
+                const user = userCredential.user;
+                return res.status(201).json({
+                    message: 'Usuário criado',
+                    data: user
+                })
+
+            }).catch((error) => {
+                return res.status(400).json({
+                    message: '[Erro FirebaseAuth]: ' + error.message
+                })
+            })
+        } catch (error) {
+            res.status(500).json({
+                message: '[Erro Controller]: ' + error
+            })
+        }
     }
 
     static async login(req: Request, res: Response) {
-        const auth = getAuth(firebaseApp)
-        const { email, password } = req.body
-        console.log(email, password);
+        try {
+            const { email, password } = req.body
+            const auth = getAuth(firebaseApp)
+            await signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
+                res.status(200).json({
+                    data: userCredential.user
+                })
 
-        // firebaseAuth.signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
-        await signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
-            res.status(200).json({
-                data: userCredential.user
+            }).catch((e) => {
+                res.status(401).json({
+                    message: '[Erro FirebaseAuth]: ' + e.message
+                })
             })
-
-        }).catch((e) => {
-            res.status(401).json({
-                message: 'Erro Controller ' + e.message
+        } catch (error) {
+            res.status(500).json({
+                message: '[Erro Controller]: ' + error
             })
-        })
+        }
     }
 }
 
--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -3,7 +3,7 @@ import UserController from "../controllers/UserController"
 
 const router = Router()
 
-router.get("/register", UserController.register)
+router.post("/register", UserController.register)
 router.get("/login", UserController.login)
 
 export default router
