969cfdf5b401788ff896dfdc0ce68d1a859c711d
viniciuzrock <vinir.santoss@gmail.com>
2023-07-05 12:41:04 -0300

    feat:configurando nodedmailer para enviar array de produtos

--- helpers/mail/mailer.ts
+++ helpers/mail/mailer.ts
@@ -1,4 +1,8 @@
 import nodemailer from "nodemailer"
+import * as fs from "fs";
+import * as path from "path";
+import handlebars from "handlebars";
+
 const user: string = 'vinir.santoss@gmail.com'
 const pass: string = 'jzudejadapwtxmhu'
 const port: number = 465
@@ -17,18 +21,24 @@ const transporter = nodemailer.createTransport({
 
 transporter.use('compile', inLineCss());
 export const sendEmailHTML = async (to: string, subject: string, body: string) => {
+
     const mailOptions = {
         from: 'no-reply@teste.com',//user,
         to,
         subject,
         html: body
     }
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log('[Erro no envio de e-mail]: ' + error)
-        } else {
-            console.log('Email sent: ' + info.response)
-        }
-    })
+
+    return new Promise<void>((resolve, reject) => {
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.log('[Erro no envio de e-mail]: ' + error);
+                reject(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+                resolve();
+            }
+        });
+    });
 }
 
